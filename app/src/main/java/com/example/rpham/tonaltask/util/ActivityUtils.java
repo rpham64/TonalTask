75c762bd114ddce865940ccd8746972a8151fec9
rpham64 <rpham64@gmail.com>
2018-04-17 01:25:37 -0700

    Removed guava dependency due to build errors. Added gson converter dependency.

--- app/src/main/java/com/example/rpham/tonaltask/util/ActivityUtils.java
+++ app/src/main/java/com/example/rpham/tonaltask/util/ActivityUtils.java
@@ -5,8 +5,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 /**
  * This utility class provides methods to help Activities load their UI.
  */
@@ -24,8 +22,6 @@ public final class ActivityUtils {
      */
     public static void addFragmentToActivity (@NonNull FragmentManager fragmentManager,
                                               @NonNull Fragment fragment, int frameId) {
-        checkNotNull(fragmentManager);
-        checkNotNull(fragment);
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.add(frameId, fragment);
         transaction.commit();
