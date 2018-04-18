ea06b5d3a32119d8d2e3f4622796057bbe3e39b3
rpham64 <rpham64@gmail.com>
2018-04-17 19:40:47 -0700

    Created Adapter and its UI for RecyclerView

---
+++ app/src/main/java/com/example/rpham/tonaltask/ui/main/adapter/ForecastsAdapter.java
@@ -0,0 +1,73 @@
+package com.example.rpham.tonaltask.ui.main.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.rpham.tonaltask.R;
+import com.example.rpham.tonaltask.data.Forecast;
+import com.example.rpham.tonaltask.data.ForecastMainInfo;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ForecastsAdapter extends RecyclerView.Adapter<ForecastsAdapter.ForecastsViewHolder> {
+
+    private Context mContext;
+    private List<Forecast> mForecastList;
+
+    public ForecastsAdapter(Context context, List<Forecast> forecastList) {
+        mContext = context;
+        mForecastList = forecastList;
+    }
+
+    @NonNull
+    @Override
+    public ForecastsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.viewholder_forecast, parent, false);
+        return new ForecastsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ForecastsViewHolder holder, int position) {
+        Forecast forecast = mForecastList.get(position);
+        holder.bindForecast(forecast);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mForecastList == null ? 0 : mForecastList.size();
+    }
+
+    class ForecastsViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.textview_max_temperature) TextView mTextViewMaxTemperature;
+        @BindView(R.id.textview_min_temperature) TextView mTextViewMinTemperature;
+        @BindView(R.id.textview_humidity) TextView mTextViewHumidity;
+
+        public ForecastsViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bindForecast(Forecast forecast) {
+            ForecastMainInfo mainInfo = forecast.getMainInfo();
+
+            // Convert max and min temperature values to String as TextViews cannot take float.
+            String textMaxTemp = String.valueOf(mainInfo.getMaxTemp());
+            String textMinTemp = String.valueOf(mainInfo.getMinTemp());
+            String textHumidity = String.valueOf(mainInfo.getHumidity());
+
+            mTextViewMaxTemperature.setText(textMaxTemp);
+            mTextViewMinTemperature.setText(textMinTemp);
+            mTextViewHumidity.setText(textHumidity);
+        }
+    }
+}
